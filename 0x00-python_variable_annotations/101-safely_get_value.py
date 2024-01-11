5ad137e7dea135c642062c8a07c8b9dbd9f40b62
leila111111 <leila_merzak@outlook.fr>
2024-01-11 23:42:10 +0000

    tasks

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,8 +3,12 @@
 from typing import Any, Mapping, Union, TypeVar
 
 
+T = TypeVar('T')
+R = Union[Any, T]
+
+
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] = None) -> Union[Any, TypeVar('T')]:
+                     default: Union[T, None] = None) -> R:
     '''extract a value from a dictionnary'''
     if key in dct:
         return dct[key]
