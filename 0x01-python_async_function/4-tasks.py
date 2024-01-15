e577d18bf652fd7edabe97b05ff35bc4de026698
leila111111 <leila_merzak@outlook.fr>
2024-01-15 23:48:04 +0000

    tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''The code is nearly identical to wait_n except task_wait_random is being called.'''
+    '''The code is nearly identical to wait_n except task_wait_random
+    is being called.'''
     wait_times = await asyncio.gather(
         *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
     )
